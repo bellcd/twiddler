11c8a4f5a280679aef6e9523d2a30f1a6e33ebd0
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-03 17:03:29 -0400

    Adds basic write tweet feature

--- data_generator.js
+++ data_generator.js
@@ -16,6 +16,10 @@ window.users = Object.keys(streams.users);
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
   var username = newTweet.user;
+
+  // believe this is necessary ...
+  streams.users[username] = [];
+
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
